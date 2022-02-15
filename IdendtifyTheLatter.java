f3768c30f4a92612d1f7782023187a1b8d263c61
muthujagadeshn <99677347+muthujagadeshn@users.noreply.github.com>
2022-02-15 17:56:47 +0530

    Add files via upload

---
+++ IdendtifyTheLatter.java
@@ -0,0 +1,18 @@
+package youtube.day1;
+
+public class IdendtifyTheLatter {
+	public static void main(String[] args) {
+		String identify="muthu jagadesh";
+		int count=0;
+		for (int i = 0; i < identify.length(); i++) {
+			if(identify.charAt(i)=='a') {
+				count++;
+				}
+			
+			
+			
+	}
+		System.out.println(count);
+	}
+
+}
