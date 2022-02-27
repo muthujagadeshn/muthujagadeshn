c5b132895bed7b5655d3db38dd0caaaf46428b9c
muthujagadeshn <99677347+muthujagadeshn@users.noreply.github.com>
2022-02-27 23:02:06 +0530

    Add files via upload

--- SnapDeal.java
+++ SnapDeal.java
@@ -1,8 +1,10 @@
-package learning.day14;
+package action;
+
+
 
 import java.time.Duration;
-import java.util.List;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,24 +15,23 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SnapDeal {
 	public static void main(String[] args) {
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions op=new ChromeOptions();
-		op.addArguments("--disable-notifications");
-		ChromeDriver driver=new ChromeDriver(op);
-		driver.get("https://www.snapdeal.com/");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		WebElement ww1 = driver.findElement(By.xpath("//div[@id='content_wrapper']//following::a//following-sibling::span[2]"));
-		Actions bbc=new Actions(driver);
-		bbc.moveToElement(ww1).perform();
-		driver.findElement(By.xpath("//div[@id='category1Data']//following::p[2]//a//span")).click();
-		List<WebElement> tp = driver.findElements(By.xpath("//div[@id='category1Data']//following::p[2]//a//span"));
-		int count=tp.size();
-		System.out.println(count);
-		String tex = driver.findElement(By.xpath("//h1[@category='Sports Shoes for Men']")).getText();
-		System.out.println(tex);
-
+		
 	
+	WebDriverManager.chromedriver().setup();
+	ChromeDriver driver=new ChromeDriver();
+	ChromeOptions option=new ChromeOptions();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	option.addArguments("--disable-notification--");
+	driver.get("https://www.snapdeal.com/");
+	 Alert alert = driver.switchTo().alert();
+  	 alert.accept();
+  	 WebElement men =driver.findElement(By.xpath("//ul[@class='nav smallNav']//span[2]"));
+  	 Actions act=new Actions(driver);
+  	 act.moveToElement(men).perform();
+  	driver.findElement(By.xpath("//span[text()='Sports Shoes']"));
+  	String count =driver.findElement(By.xpath("//span[@class='category-name category-count']")).getTagName();
+  	System.out.println(count);
 	}
-
 }
+
